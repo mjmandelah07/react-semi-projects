b27379cd4ba83322833ee94a9af8caefc3b616f7
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-05-29 06:45:09 +0100

    styled with bootstrap

--- counterapp/src/Counter.js
+++ counterapp/src/Counter.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import styles from './my-style.module.css'
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import './counter.css'
+
+
 
 function Counter() {
   const [count, setCount] = useState(0);
@@ -12,15 +20,15 @@ function Counter() {
   };
 
     return (
-      <>
+      <div className={`container text-center justify-content-center align-content-center`}>
       <h1>Counter</h1>
-    <div className={styles.counter}>
+    <div className={`border border-5 py-5 border-black`}>
       <p>Count: { count }</p>
       <button onClick={ increment }>Click to increase</button>
       <button onClick={ decrement }>Click to decrease</button>
 
     </div>
-    </>
+    </div>
   );
 }
 
